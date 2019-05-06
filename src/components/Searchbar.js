34b31e076f4ab0be74cfff662a4b23edab11ace9
Pierre Lambert <supagoof924@hotmail.com>
2019-05-06 21:51:55 +0200

    [ FEATURE ] Updated Searchbar to be a controlled element

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,18 +1,23 @@
 import React from "react";
 
 class Search extends React.Component {
-  onHandleChange = (event) => {
-    console.log(event.target.value)
-  }
+  state = {
+    term: ""
+  };
 
-  
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>Image search</label>
-            <input type="text" onClick={this.onHandleClick} onChange={this.onHandleChange}/>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={e => 
+                this.setState({ term: e.target.value })
+              }
+            />
           </div>
         </form>
       </div>
