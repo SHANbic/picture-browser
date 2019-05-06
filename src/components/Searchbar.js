fc70a879c475d966796cd28425532542e929bf67
Pierre Lambert <supagoof924@hotmail.com>
2019-05-06 21:39:53 +0200

    [ FEATURE ] Binded a onChange function to Searchbar

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,13 +1,18 @@
 import React from "react";
 
 class Search extends React.Component {
+  onHandleChange = (event) => {
+    console.log(event.target.value)
+  }
+
+  
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>Image search</label>
-            <input type="text" />
+            <input type="text" onClick={this.onHandleClick} onChange={this.onHandleChange}/>
           </div>
         </form>
       </div>
