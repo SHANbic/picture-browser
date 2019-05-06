aa827f49c60a1720812e1e6da99d491923d108e1
Pierre Lambert <supagoof924@hotmail.com>
2019-05-06 21:34:24 +0200

    [ Feature ] Added Searchbar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Search from "./Searchbar";
 
 const App = () => {
-  return <div>Hello World !</div>;
+  return (
+    <div className="ui container" style={{marginTop:'1rem'}}>
+      <Search />
+    </div>
+  );
 };
 
 export default App;
