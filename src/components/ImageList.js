5a7a7e6c090b12d155234ee4af2463a8b6896d22
Pierre Lambert <supagoof924@hotmail.com>
2019-05-07 12:35:08 +0200

    [ FEATURE ] Updated ImageList with alt and id

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,9 +2,13 @@ import React from "react";
 
 const ImageList = props => {
   const imageList = props.images.map(image => {
-    return <li><img src={image.urls.small} /></li>;
+    return (
+      <li key={image.id}>
+        <img src={image.urls.small} alt={image.alt_description} />
+      </li>
+    );
   });
-  return <ul style={{listStyleType:'none'}}>{imageList}</ul>;
+  return <ul style={{ listStyleType: "none" }}>{imageList}</ul>;
 };
 
 export default ImageList;
