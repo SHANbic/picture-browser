436cd261be50cea0372c2ce00a0751fd8fa1df2d
Pierre Lambert <supagoof924@hotmail.com>
2019-05-07 10:25:12 +0200

    [ FEATURE ] Added axios module to make GET requests on unsplash API from App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,22 @@
 import React from "react";
+import axios from "axios";
 import Search from "./Searchbar";
 
 class App extends React.Component {
-  onSearchbarSubmit = term => {
-    console.log(term);
+  onSearchSubmit = term => {
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID 45cea227ff8d63eeb7762c3daf343ba1db929e1b9b11e642e50dd166ff4e0101"
+      }
+    });
   };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "1rem" }}>
-        <Search onSubmit={this.onSearchbarSubmit}/>
+        <Search onSubmit={this.onSearchSubmit} />
       </div>
     );
   }
