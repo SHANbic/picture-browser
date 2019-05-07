1654fafdbed9307545981a2e7c8013d9da442ab8
Pierre Lambert <supagoof924@hotmail.com>
2019-05-07 11:46:18 +0200

    [ FEATURE ] Added Unslash component to handle core api request

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,12 @@
 import React from "react";
-import axios from "axios";
+import unsplash from "../api/unsplash";
 import Search from "./Searchbar";
 
 class App extends React.Component {
   state = { images: [] };
   onSearchSubmit = async term => {
-    const response = await axios.get("https://api.unsplash.com/search/photos", {
+    const response = await unsplash.get("/search/photos", {
       params: { query: term },
-      headers: {
-        Authorization:
-          "Client-ID 45cea227ff8d63eeb7762c3daf343ba1db929e1b9b11e642e50dd166ff4e0101"
-      }
     });
     this.setState({ images: response.data.results });
   };
