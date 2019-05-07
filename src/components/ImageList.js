5d52242c48a88d03dd0e75256ad414381091e827
Pierre Lambert <supagoof924@hotmail.com>
2019-05-07 14:43:27 +0200

    [ FEATURE ] Added ImageCard component

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,10 @@
-import React from "react";
 import "./ImageList.css";
+import React from "react";
+import ImageCard from "./ImageCard";
 
 const ImageList = props => {
   const images = props.images.map(image => {
-    return (
-      <img key={image.id} src={image.urls.small} alt={image.alt_description} />
-    );
+    return <ImageCard key={image.id} image={image} />;
   });
   return <div className="image-list">{images}</div>;
 };
